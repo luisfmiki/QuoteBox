a926582ba9c5e0ee400bec427e7443e5a630ed97
luisfmiki@hotmail.com <luis@pop-os.localdomain>
2020-10-15 20:05:24 -0300

    Updates

---
+++ precache-manifest.f9a3d21efd615d6c6708928e39579225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0c26c5a0b512e2fa715a9b645e461a",
+    "url": "/QuoteBox/index.html"
+  },
+  {
+    "revision": "824d4613e8cea88b50c9",
+    "url": "/QuoteBox/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db535150fb711c8e5a0c",
+    "url": "/QuoteBox/static/js/2.1b625caa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/QuoteBox/static/js/2.1b625caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "824d4613e8cea88b50c9",
+    "url": "/QuoteBox/static/js/main.9b25aa6b.chunk.js"
+  },
+  {
+    "revision": "dbe29c41d9ba099d76f7",
+    "url": "/QuoteBox/static/js/runtime-main.b7fc31a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/QuoteBox/static/media/logo.5d5d9eef.svg"
+  }
+]);
